1fb0dcfd90d796793118d917fe66d321e6355b5f
SingleCorner <sinerwr@gmail.com>
2017-08-15 15:28:38 +0800

    fixed_raven_init

--- controller/main.go
+++ controller/main.go
@@ -9,6 +9,7 @@ Email:    sinerwr@gmail.com
 package controller
 
 import (
+	"github.com/getsentry/raven-go"
 	"google.golang.org/grpc"
 
 	"github.com/SiCo-Ops/Pb"
@@ -23,7 +24,12 @@ func init() {
 	defer func() {
 		recover()
 	}()
+
 	mongo.AAAEnsureIndexes()
 	pb.RegisterAAAPublicServiceServer(RPCServer, &AAAPublicService{})
 	pb.RegisterAAAPrivateServiceServer(RPCServer, &AAAPrivateService{})
+
+	if config.Sentry.Enable {
+		raven.SetDSN(config.Sentry.DSN)
+	}
 }
